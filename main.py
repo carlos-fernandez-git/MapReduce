bfc343d49407dc63711dc5b778fcf4bd9892bf7b
carlos-fernandez-git <72436949+carlos-fernandez-git@users.noreply.github.com>
2021-03-18 17:55:47 +0100

    Cambios en estructura del programa

--- main.py
+++ main.py
@@ -2,46 +2,7 @@ import sys
 import re
 import threading
 import time
-
-
-def reduce(shared_dict):
-    if threading.current_thread().name == "Thread3":
-        for i in sorted(shared_dict)[0: int(len(shared_dict) / 2)]:
-            shared_dict[i] = sum(shared_dict[i])
-
-    if threading.current_thread().name == "Thread4":
-        for j in sorted(shared_dict)[int(len(shared_dict) / 2):]:
-            shared_dict[j] = sum(shared_dict[j])
-
-
-# Iteramos sobre la lista, si la clave no está, se crea una lista con [1], si la clave existe, se añade 1 a la lista
-def shuffle(shared_dict, list_thread1, list_thread2):
-    if threading.current_thread().name == "Thread 1":
-        for word in list_thread1:
-            if word[0] in shared_dict:
-                shared_dict[word[0]].append(1)
-            else:
-                shared_dict[word[0]] = [1]
-    if threading.current_thread().name == "Thread 2":
-        for word in list_thread2:
-            if word[0] in shared_dict:
-                shared_dict[word[0]].append(1)
-            else:
-                shared_dict[word[0]] = [1]
-
-
-def mapping(texto, list_thread1, list_thread2, shared_dict):
-    if threading.current_thread().name == "Thread 1":
-        palabras = texto.split()
-        for palabra in palabras:
-            list_thread1.append([palabra, 1])
-
-    if threading.current_thread().name == "Thread 2":
-        palabras = texto.split()
-        for palabra in palabras:
-            list_thread2.append([palabra, 1])
-
-    shuffle(shared_dict, list_thread1, list_thread2)
+import logging
 
 
 # Leemos el archivo de texto (input)
@@ -63,44 +24,79 @@ def splitting(input_text):
     # sub(caracteres_a_buscar, reemplazo, texto)
     input_text = re.sub('[,.;:!¡?¿()]+', '', input_text)
 
-    # Dividimos el texto en dos partes
-    primera_parte, segunda_parte = input_text[:int(len(input_text) / 2)], input_text[int(len(input_text) / 2):]
+    list_all_words = input_text.split()
+    list_of_ones = []
+    n_threads = 6
+    threads = list()
+    # Creamos los threads y llamamos a mapping
+    for i in range(n_threads):
+        x = threading.Thread(target=mapping, args=(i, list_all_words, n_threads, list_of_ones))
+        threads.append(x)
+        x.start()
 
-    list_thread1 = []
-    list_thread2 = []
-    # Dividimos el texto entre los diferentes threads
-    shared_dict = {}
-    t1 = threading.Thread(name="Thread 1", target=mapping,
-                          args=(primera_parte, list_thread1, list_thread2, shared_dict))
-    t2 = threading.Thread(name="Thread 2", target=mapping,
-                          args=(segunda_parte, list_thread1, list_thread2, shared_dict))
+    for index, thread in enumerate(threads):
+        thread.join()
 
-    t1.start()
-    t2.start()
+    # Creamos los threads y llamamos a Shuffle
+    shuffle_dict_result = {}
+    for i in range(n_threads):
+        x = threading.Thread(target=shuffle, args=(list_of_ones, shuffle_dict_result, n_threads, i, threading.Lock()))
+        threads.append(x)
+        x.start()
 
-    t1.join()
-    t2.join()
+    for index, thread in enumerate(threads):
+        thread.join()
 
-    # Volvemos a crear nuevos threads para la parte de reduce
-    t3 = threading.Thread(name="Thread3", target=reduce, args=(shared_dict,))
-    t4 = threading.Thread(name="Thread4", target=reduce, args=(shared_dict,))
+    # Convertimos el diccionario a lista para poder recorrerlo con indice
+    list_key_value = list(shuffle_dict_result.items())
+    dict_final_result = {}
+    # Parte de reduce:
+    for i in range(n_threads):
+        x = threading.Thread(target=reduce, args=(list_key_value, n_threads, i, dict_final_result))
+        threads.append(x)
+        x.start()
 
-    t3.start()
-    t4.start()
+    for index, thread in enumerate(threads):
+        thread.join()
 
-    t3.join()
-    t4.join()
 
-    return shared_dict
+
+def mapping(index, list_all_words, n_threads, list_of_ones):
+    for i in range(int((index / n_threads) * len(list_all_words)),
+                   int(((index + 1) / n_threads) * len(list_all_words))):
+        list_of_ones.append([list_all_words[i], 1])
+
+
+def shuffle(list_of_ones, shuffle_dict_result, n_threads, index, lock):
+    for i in range(int((index / n_threads) * len(list_of_ones)),
+                   int(((index + 1) / n_threads) * len(list_of_ones))):
+        if list_of_ones[i][0] in shuffle_dict_result:
+            lock.acquire()
+            shuffle_dict_result[list_of_ones[i][0]].append(1)
+            lock.release()
+        else:
+            lock.acquire()
+            shuffle_dict_result[list_of_ones[i][0]] = [1]
+            lock.release()
+
+
+def reduce(list_key_value, n_threads, index, dict_final_reduced):
+    for i in range(int((index / n_threads) * len(list_key_value)),
+                   int(((index + 1) / n_threads) * len(list_key_value))):
+        dict_final_reduced[list_key_value[i][0]] = sum(list_key_value[i][1])
 
 
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("No se ha especificado ningún archivo de texto...")
+        sys.exit()
     start_time = time.time()
 
     for index in range(1, len(sys.argv)):
         print(sys.argv[index], ":", sep="")
         text = input(index)
-        result_dictionary_words = splitting(text)
-        #for word in result_dictionary_words:
-         #   print(word, ':', result_dictionary_words[word])
+        splitting(text)
+        # for word in result_dictionary_words:
+        #   print(word, ':', result_dictionary_words[word])
+
     print("--- %s seconds ---" % (time.time() - start_time))
