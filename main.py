1cf1d226809f0e39800b333f0b4f6e7d2bc30699
carlos-fernandez-git <72436949+carlos-fernandez-git@users.noreply.github.com>
2021-03-24 18:06:35 +0100

    Nueva versión (map, shuffle) con Multiprocessing

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import sys
 import re
-import threading
+import concurrent.futures
+from multiprocessing import Manager
+import multiprocessing
 import time
-import logging
+import tempfile
+import os
 
 
-# Leemos el archivo de texto (input)
+# Read text file
 def input(index):
     try:
         reader = open(sys.argv[index], "r", encoding="utf8")
     except OSError:
-        print("El archivo no se ha podido abrir correctamente")
+        print("Error")
         sys.exit()
 
     texto = reader.read()
@@ -18,77 +21,44 @@ def input(index):
     return texto
 
 
-# Convertimos el texto a lista de palabras
+# Convert text to list of words
 def splitting(input_text):
     input_text = input_text.lower()
-    # sub(caracteres_a_buscar, reemplazo, texto)
     input_text = re.sub('[,.;:!¡?¿()]+', '', input_text)
+    words = input_text.split()
+    n_processes = 4
+    # Creating processes
+    word_map_result = []
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = []
+        for id_process in range(n_processes):
+            results.append(executor.submit(mapping, words, n_processes, id_process, word_map_result))
 
-    list_all_words = input_text.split()
-    list_of_ones = []
-    n_threads = 6
-    threads = list()
-    # Creamos los threads y llamamos a mapping
-    for i in range(n_threads):
-        x = threading.Thread(target=mapping, args=(i, list_all_words, n_threads, list_of_ones))
-        threads.append(x)
-        x.start()
-
-    for index, thread in enumerate(threads):
-        thread.join()
-
-    # Creamos los threads y llamamos a Shuffle
-    shuffle_dict_result = {}
-    for i in range(n_threads):
-        x = threading.Thread(target=shuffle, args=(list_of_ones, shuffle_dict_result, n_threads, i, threading.Lock()))
-        threads.append(x)
-        x.start()
-
-    for index, thread in enumerate(threads):
-        thread.join()
-
-    # Convertimos el diccionario a lista para poder recorrerlo con indice
-    list_key_value = list(shuffle_dict_result.items())
-    dict_final_result = {}
-    # Parte de reduce:
-    for i in range(n_threads):
-        x = threading.Thread(target=reduce, args=(list_key_value, n_threads, i, dict_final_result))
-        threads.append(x)
-        x.start()
-
-    for index, thread in enumerate(threads):
-        thread.join()
+        for f in concurrent.futures.as_completed(results):
+            print(f.result())
 
 
+def mapping(words, n_processes, id_process, word_map_result):
+    for i in range(int((id_process / n_processes) * len(words)),
+                   int(((id_process + 1) / n_processes) * len(words))):
+        word_map_result.append([words[i], 1])
 
-def mapping(index, list_all_words, n_threads, list_of_ones):
-    for i in range(int((index / n_threads) * len(list_all_words)),
-                   int(((index + 1) / n_threads) * len(list_all_words))):
-        list_of_ones.append([list_all_words[i], 1])
+    return shuffle(word_map_result)
 
 
-def shuffle(list_of_ones, shuffle_dict_result, n_threads, index, lock):
-    for i in range(int((index / n_threads) * len(list_of_ones)),
-                   int(((index + 1) / n_threads) * len(list_of_ones))):
-        if list_of_ones[i][0] in shuffle_dict_result:
-            lock.acquire()
-            shuffle_dict_result[list_of_ones[i][0]].append(1)
-            lock.release()
+def shuffle(word_map_result):
+    result_dict = {}
+    for word in word_map_result:
+        if word[0] in result_dict:
+            result_dict[word[0]].append(1)
         else:
-            lock.acquire()
-            shuffle_dict_result[list_of_ones[i][0]] = [1]
-            lock.release()
-
-
-def reduce(list_key_value, n_threads, index, dict_final_reduced):
-    for i in range(int((index / n_threads) * len(list_key_value)),
-                   int(((index + 1) / n_threads) * len(list_key_value))):
-        dict_final_reduced[list_key_value[i][0]] = sum(list_key_value[i][1])
+            result_dict[word[0]] = [1]
+    return result_dict
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        print("No se ha especificado ningún archivo de texto...")
+        print("Please, specify a text file...")
         sys.exit()
     start_time = time.time()
 
